afd8ccdc2cc9d89ad6d86eae9d6f220126051ad4
paracelsus <paracelsus@b210c86f-3928-0410-9586-21d2fe3a92fe>
2007-02-01 02:18:21 +0000

    Added SWFShape class that supports reading, writing and creating the simplified shapes in fonts. Added shape loading to SWFFont, still needs to read layout info. Also added support for creating SWFFonts from scratch (also not quite done).

---
+++ SWFShape.h
@@ -0,0 +1,43 @@
+#import <Foundation/Foundation.h>
+
+#import "SWFGeometry.h"
+#import "CSHandle.h"
+
+@class SWFParser,SWFWriter;
+
+@interface SWFShape:NSObject
+{
+	NSMutableArray *shaperecords;
+}
+
+-(id)init;
+-(id)initWithHandle:(CSHandle *)fh;
+-(void)dealloc;
+
+-(void)moveTo:(SWFPoint)point;
+-(void)lineTo:(SWFPoint)point;
+-(void)curveTo:(SWFPoint)point control:(SWFPoint)control;
+
+-(void)writeToHandle:(CSHandle *)fh;
+
+@end
+
+#define SWFShapeMoveRecord 0
+#define SWFShapeLineRecord 1
+#define SWFShapeCurveRecord 2
+
+@interface SWFShapeRecord:NSObject
+{
+	int type,fill;
+	SWFPoint point,control;
+}
+
+-(id)initWithType:(int)t point:(SWFPoint)p;
+-(id)initWithType:(int)t point:(SWFPoint)p control:(SWFPoint)c;
+
+-(int)type;
+-(int)fill;
+-(SWFPoint)point;
+-(SWFPoint)control;
+
+@end
