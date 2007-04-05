15fa98a0cbc8cb43f34a20100322fe66fd6f2c73
astrange <astrange@b210c86f-3928-0410-9586-21d2fe3a92fe>
2007-04-05 23:12:29 +0000

    Obey SSA times.

--- swfinsertssa.m
+++ swfinsertssa.m
@@ -8,6 +8,14 @@
 #import "CSMemoryHandle.h"
 #import "SSADocument.h"
 
+static void WriteLineEnd(SWFWriter *writer)
+{
+	CSHandle *fh=[writer handle];
+	[writer startTag:SWFRemoveObject2Tag];
+	[fh writeUInt16LE:100]; // depth
+	[writer endTag];
+}
+
 int main(int argc,char **argv)
 {
 	NSAutoreleasePool *pool=[[NSAutoreleasePool alloc] init];
@@ -18,9 +26,13 @@ int main(int argc,char **argv)
 	SWFWriter *writer=[SWFWriter writerForPath:[NSString stringWithUTF8String:argv[2]] parser:parser];
 	SSADocument *ssa =[[SSADocument alloc] init];
 	SWFFont *font = nil;
+	SubLine *line = nil;
 	
 	[ssa loadFile:[NSString stringWithUTF8String:argv[3]] width:640 height:480];
-	int scount = [ssa packetCount];
+	int scount = [ssa packetCount], wroteFont = NO, curpacket = 0;
+	
+	if (scount == 0) return 1;
+	line = [ssa packet:curpacket++];
 	
 	while([parser nextTag])
 	{
@@ -54,44 +66,44 @@ int main(int argc,char **argv)
 			case SWFShowFrameTag:
 			{
 				int frame = [parser frame];
-				if(frame==5)
+				if (!wroteFont)
 				{
+					wroteFont=YES;
 					font=[[SWFFont alloc] initWithFilename:[@"~/mikachan-p.otf" stringByExpandingTildeInPath]
-												   fontName:@"Mikachan-P" characterSet:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(32,127-32)]
-												 identifier:0x1242];
+												  fontName:@"Mikachan-P" characterSet:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(32,127-32)]
+												identifier:0x1242];
 					
 					[font write:writer];
-				} else if (frame >= (scount - 5)-2) {
-					CSHandle *fh=[writer handle];
-					[writer startTag:SWFRemoveObject2Tag];
-					[fh writeUInt16LE:100]; // depth
-					[writer endTag];
-				} else if (frame > 5) {
-					SubLine *sline = [ssa packet:frame-5];
-					if (frame > 6) {
-						CSHandle *fh=[writer handle];
-						[writer startTag:SWFRemoveObject2Tag];
-						[fh writeUInt16LE:100]; // depth
-						[writer endTag];
+				}
+								
+				if (line) {
+					double time = [parser time] * 100.;
+					
+					if (time > line->end_time) {
+						WriteLineEnd(writer);
+						if (curpacket == scount-1) line = nil;
+						else line = [ssa packet:curpacket++];
 					}
 					
-					SWFText *text=[[[SWFText alloc] initWithObjectIdentifier:0x1999 + frame - 5] autorelease];
-					[text setRect:[parser rect]];
-					
-					[text addTextRecord:[SWFTextRecord recordWithText:[sline plaintext]
-																 font:font height:500 position:SWFMakePoint(1000,1000) red:255 green:255 blue:255 alpha:255]];
-					
-					[text write:writer];
-					
-					CSHandle *fh=[writer handle];
-					[writer startTag:SWFPlaceObject2Tag];
-					[fh writeUInt8:2|4]; // add object, with matrix
-					[fh writeUInt16LE:100]; // depth
-					[fh writeUInt16LE:[text identifier]];
-					SWFWriteMatrix(SWFRotationMatrix(0),fh);
-					[writer endTag];
+					if (time >= line->begin_time) {					
+						SWFText *text=[[[SWFText alloc] initWithObjectIdentifier:0x1999 + frame] autorelease];
+						[text setRect:[parser rect]];
+						
+						[text addTextRecord:[SWFTextRecord recordWithText:[line plaintext]
+																	 font:font height:500 position:SWFMakePoint(50,[parser rect].height - 1000) red:255 green:255 blue:255 alpha:255]];
+						
+						[text write:writer];
+						
+						CSHandle *fh=[writer handle];
+						[writer startTag:SWFPlaceObject2Tag];
+						[fh writeUInt8:2|4]; // add object, with matrix
+						[fh writeUInt16LE:100]; // depth
+						[fh writeUInt16LE:[text identifier]];
+						SWFWriteMatrix(SWFRotationMatrix(0),fh);
+						[writer endTag];
+					}
 				}
-				
+								
 				[writer startTag:SWFShowFrameTag length:0];
 				[writer endTag];
 			}
